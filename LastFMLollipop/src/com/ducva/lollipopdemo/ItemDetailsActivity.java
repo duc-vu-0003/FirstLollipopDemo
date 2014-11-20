857c7dfbc805ec34a7d83160e0e231fc2bf3c256
DUCVA <hex0r@me.com>
2014-11-20 10:27:56 +0700

    Add Palette demo

--- LastFMLollipop/src/com/ducva/lollipopdemo/ItemDetailsActivity.java
+++ LastFMLollipop/src/com/ducva/lollipopdemo/ItemDetailsActivity.java
@@ -2,9 +2,13 @@ package com.ducva.lollipopdemo;
 
 import android.animation.Animator;
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.graphics.Palette.PaletteAsyncListener;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
@@ -13,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ducva.lollipopdemo.model.BaseModel;
+import com.ducva.lollipopdemo.utils.AppUtils;
 import com.ducva.lollipopdemo.utils.DebugLog;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -70,6 +75,8 @@ public class ItemDetailsActivity extends ActionBarActivity {
 			View view = rowContainer.findViewById(R.id.row_image);
 			ImageView iv = (ImageView) view.findViewById(R.id.img_details);
             ImageLoader.getInstance().displayImage(item.imageLink, iv);
+            
+            extract(item.imageLink);
             
             DebugLog.v(item.imageLink);
 			
@@ -144,5 +151,25 @@ public class ItemDetailsActivity extends ActionBarActivity {
                 }
             });
         }
+	}
+	
+	private void extract(String imageLink) {
+		Bitmap bitmap = ImageLoader.getInstance().loadImageSync(imageLink, AppUtils.getOptionsQualityCache());
+		
+		Palette.generateAsync(bitmap, new PaletteAsyncListener() {
+			
+			@Override
+			public void onGenerated(Palette palette) {
+				// TODO Auto-generated method stub
+                Palette.Swatch vibrant = palette.getVibrantSwatch();   
+                Palette.Swatch darkVibrant = palette.getDarkVibrantSwatch();   
+                Palette.Swatch lightVibrant = palette.getLightVibrantSwatch();  
+                Palette.Swatch muted = palette.getMutedSwatch();   
+                Palette.Swatch darkMuted = palette.getDarkMutedSwatch();   
+                Palette.Swatch lightMuted = palette.getLightMutedSwatch();   
+                
+                toolbar.setBackgroundDrawable(new ColorDrawable(vibrant.getRgb()));
+			}
+		});
 	}
 }
